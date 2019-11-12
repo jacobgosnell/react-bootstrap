1fb825f4ff4d9ae5d967e8f6c23a3d254a03a1de
Jake <gosnelljacob@gmail.com>
2019-11-11 22:16:12 -0800

    added files to git

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <p>Put it in, leave it alone. This is gonna be a happy little seascape. Be careful. You can always add more - but you can't take it away. Just use the old one inch brush.</p>
+      <p>A tree needs to be your friend if you're going to paint him. We'll throw some happy little limbs on this tree. And maybe, maybe, maybe...
+      </p>
+      <p>Anyone can paint. Get tough with it, get strong. We'll make some happy little bushes here. Don't be afraid to make these big decisions. Once you start, they sort of just make themselves.</p>
+    </div>
+  );
+}
